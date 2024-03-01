d0b8e63cf1d30123bf81f46ace2398b7d65e80ae
shreyasingh1354 <133848560+shreyasingh1354@users.noreply.github.com>
2024-03-01 12:58:49 +0530

    newCommit

---
+++ staking/src/components/Navigation/ConnectedNetwork.jsx
@@ -0,0 +1,17 @@
+import React, { useContext } from 'react'
+import Web3Context from '../../context/Web3context'
+
+const ConnectedNetwork = () => {
+    const {chainId}=useContext(Web3Context);
+   if(chainId===null){
+      return <p className="network">Not connected</p>;
+   }
+   else if (chainId === 11155111) {
+      return <p className="network">Sepolia</p>;
+    } else {
+      return <p className="network"> Network Not Detected</p>;
+    }
+  
+}
+
+export default ConnectedNetwork
